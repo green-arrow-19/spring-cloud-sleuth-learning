dd19d780886bc77c0e175f947a46fa0d585dcd1b
nakulGoyal19 <nakul19goyal@gmail.com>
2020-04-23 15:29:17 +0530

    starter project initialised for server and client

---
+++ server/src/main/java/com/greenarror19/springcloudsleuth/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.greenarror19.springcloudsleuth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/greenarror19/springcloudsleuth/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenarror19.springcloudsleuth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
