fb8d6b21f9a4951f214bbeb14f46d7d63150ede1
nakulGoyal19 <nakul19goyal@gmail.com>
2020-04-23 18:03:32 +0530

    client error fixed- application running successfully

--- client/src/main/java/com/greenarror19/springcloudsleuth/client/controllers/ClientController.java
+++ client/src/main/java/com/greenarror19/springcloudsleuth/client/controllers/ClientController.java
@@ -1,5 +1,6 @@
 package com.greenarror19.springcloudsleuth.client.controllers;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,20 +8,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.logging.Logger;
 @RestController
 @RequestMapping("/client")
 public class ClientController {
     @Autowired
     private RestTemplate restTemplate;
 
-    private static final Logger LOGGER= (Logger) LoggerFactory.getLogger(ClientController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientController.class);
 
     @GetMapping("/getData")
     public String getData(){
+        LOGGER.info("Getting data form client");
         LOGGER.info("Calling server 1 form client");
         String temp = restTemplate.getForObject("http://localhost:8082/server1/getData",String.class);
-        LOGGER.info("Returning data from server 1");
+        LOGGER.info("Returning data from client");
         return "In-client-micro-service "+temp;
     }
 }
